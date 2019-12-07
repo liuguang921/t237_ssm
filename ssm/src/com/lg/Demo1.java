20678c847e41781d1b576de9a28b9b608abb7b18
liuguang921 <913551171@qq.com>
2019-12-07 12:54:12 +0800

    Initial commit

---
+++ ssm/src/com/lg/Demo1.java
@@ -0,0 +1,13 @@
+package com.lg;
+
+/**
+ * @author 废废
+ * @site
+ * @company xxx公司
+ * @create 2019-12-07 11:42
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("项目搭建完毕，该干活了");
+    }
+}
